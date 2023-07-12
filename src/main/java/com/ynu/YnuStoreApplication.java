569d4c5567c73b100f22a42b70dc1c6080e11639
LthreeC <514676651@qq.com>
2023-07-12 16:50:25 +0800

    完成前端静态页面调用

---
+++ src/main/java/com/ynu/YnuStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ynu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnuStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YnuStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ynu/YnuStoreApplicationTests.java
@@ -0,0 +1,29 @@
+package com.ynu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class YnuStoreApplicationTests {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+    }
+
+    /**
+     * 数据库连接池 DBCP C3P0 Hikari据说世界最快
+     * HikariProxyConnection@668707379 wrapping com.mysql.cj.jdbc.ConnectionImpl@65d73bd
+     * @throws SQLException
+     */
+    @Test
+    void getConnection() throws SQLException {
+        System.out.println(dataSource.getConnection());
+    }
+
+}
