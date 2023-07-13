e9c5f0e0f1b971968e5cb4d51c44c2dab4e1ad5d
LthreeC <514676651@qq.com>
2023-07-13 12:03:54 +0800

    完成拦截器，浏览器不能一直重定向

---
+++ src/main/java/com/ynu/config/LoginInterceptorConfigurer.java
@@ -0,0 +1,37 @@
+package com.ynu.config;
+
+import com.ynu.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration // 加载当前类到springboot容器中
+public class LoginInterceptorConfigurer implements WebMvcConfigurer {
+    // 配置白名单
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        HandlerInterceptor interceptor = new LoginInterceptor();
+        List<String> patterns = new ArrayList<>();
+        patterns.add("/bootstrap3/**");
+        patterns.add("/css/**");
+        patterns.add("/images/**");
+        patterns.add("/js/**");
+        patterns.add("/web/register.html");
+        patterns.add("/web/login.html");
+        patterns.add("/web/index.html");
+        patterns.add("/web/product.html");
+        patterns.add("/users/reg");
+        patterns.add("/users/login");
+
+
+        // 完成拦截器的注册
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(patterns);
+    // 表示要拦截的url是什么
+    }
+}
